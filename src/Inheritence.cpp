a2e01953ce96c1f6e59559817fbe58ebe0303be8
daishan <daishan_ustc_cs@163.com>
2021-03-22 22:46:52 +0800

    根据代码证明了父类指针或者引用，只能从子类对象中看到自己的部分，无法看到子类特有的部分

--- src/Inheritence.cpp
+++ src/Inheritence.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<string>
+#include<string_view>
 class Base{
 protected:
     int m_value;
@@ -33,7 +34,41 @@ public:
     }
 };
 // 注意在同一个系统中由于多个libstdc++-6.dll产生的编译器无法选择问题
+
+class Animal{
+protected:
+    std::string m_name;
+    Animal(std::string name):m_name(name){
+
+    }
+    // prevent slicing(covered later)
+    Animal(const Animal&) = delete;
+    Animal& operator=(const Animal&) = delete;
+public:
+    const std::string& getName()const{return m_name;}
+    std::string speak()const{return "???";}
+};
+
+class Cat:public Animal{
+public:
+    Cat(std::string name):Animal(name){
+
+    }
+    std::string speak()const {return "Meow";}
+};
+
+class Dog:public Animal{
+public:
+    Dog(std::string name)
+        :Animal(name){
+
+    }
+    
+    std::string speak()const{return "Woof";}
+};
+
 int main(){
+    #if 0 
     Derived d(12);
     std::cout<<d.GetName()<<":"<<d.GetValue()<<std::endl;
 
@@ -42,5 +77,32 @@ int main(){
 
     Derived* pd = &d;
     std::cout<<pd->GetName()<<":"<<pd->GetValue()<<std::endl;
+    #endif
+
+#if 0 
+    Derived derived{5};
+    Base &rbase = derived;
+    Base *pbase = &derived;
+    // 通过基类的指针和引用，指向派生类的对象，我们可以访问派生类对象的基类部分
+    // 在没有virtual的情况下，基类的指针和引用都看不到子类的数据和对象，只能
+    // 看到子类对象的基类部分的数据和函数
+    std::cout<<"derived is a"<<derived.GetName()<<" and has value "<<derived.GetValue()<<"\n";
+    std::cout<<"rBase is a "<<rbase.GetName()<<" and has value "<<rbase.GetValue()<<"\n";
+    std::cout<<"pBase is a "<<pbase->GetName()<<" and has value "<<pbase->GetValue()<<"\n"; 
     std::cin.get();
+#endif
+
+    const Cat cat("Fred");
+    std::cout<<"cat is named "<<cat.getName()<<", and it says "<<cat.speak()<<"\n";
+    
+    const Dog dog("Garbo");
+    std::cout<<"dog is named "<<dog.getName()<<", and it says "<<dog.speak()<<"\n";
+
+    const Animal *pAnimal{&cat};
+    std::cout<<"pAnimal is named "<<pAnimal->getName()<<", and it says "<<pAnimal->speak()<<"\n";
+
+    pAnimal = &dog;
+    std::cout<<"pAnimal is named "<<pAnimal->getName()<<", and it says "<<pAnimal->speak()<<"\n";
+    std::cin.get();
+    return 0;
 }
